e3f3218e9a486eb91bf20cd30cd90c004d528a5b
Drekken <drekken.pownz@protonmail.com>
2024-04-22 20:24:20 -0400

    Revert "setup new bot, changed name and settings"
    This reverts commit a8c800f490e9d7002312b0e733e96be6cd4d54f0.

--- run.py
+++ run.py
@@ -8,8 +8,6 @@ from sc2 import maps
 from sc2.data import AIBuild, Difficulty, Race
 from sc2.main import run_game
 from sc2.player import Bot, Computer
-from sc2.bot_ai import BotAI
-
 
 sys.path.append("ares-sc2/src/ares")
 sys.path.append("ares-sc2/src")
@@ -17,34 +15,21 @@ sys.path.append("ares-sc2")
 
 import yaml
 
-from bot.main import AnglerBot
+from bot.main import MyBot
 from ladder import run_ladder_game
 
 # change if non default setup / linux
 # if having issues with this, modify `map_list` below manually
-MAPS_PATH: str = "D:\\StarCraft II\\Maps"
+MAPS_PATH: str = "C:\\Program Files (x86)\\StarCraft II\\Maps"
 CONFIG_FILE: str = "config.yml"
 MAP_FILE_EXT: str = "SC2Map"
 MY_BOT_NAME: str = "MyBotName"
 MY_BOT_RACE: str = "MyBotRace"
 
-class DummyBot(BotAI):
-    def __init__(self):
-        super().__init__()
-
-    async def on_step(self, iteration: int):
-        target = self.game_info.map_center
-        if self.enemy_start_locations:
-            target = self.enemy_start_locations[0]
-        elif self.enemy_structures:
-            target = self.enemy_structures.first.position
-        for unit in self.units.idle:
-            unit.attack(target)
-
 
 def main():
     bot_name: str = "MyBot"
-    race: Race = Race.Protoss
+    race: Race = Race.Random
 
     __user_config_location__: str = path.abspath(".")
     user_config_path: str = path.join(__user_config_location__, CONFIG_FILE)
@@ -57,9 +42,7 @@ def main():
             if MY_BOT_RACE in config:
                 race = Race[config[MY_BOT_RACE].title()]
 
-    bot1 = Bot(race, AnglerBot(), bot_name)
-    bot2 = Bot(Race.Random, DummyBot())
-
+    bot1 = Bot(race, MyBot(), bot_name)
 
     if "--LadderServer" in sys.argv:
         # Ladder game started by LadderManager
@@ -69,10 +52,19 @@ def main():
     else:
         # Local game
         map_list: List[str] = [
-             "PlateauMicro_1",
-             "BotMicroArena_6",
+            p.name.replace(f".{MAP_FILE_EXT}", "")
+            for p in Path(MAPS_PATH).glob(f"*.{MAP_FILE_EXT}")
+            if p.is_file()
         ]
-        
+        # alternative example code if finding the map path is problematic
+        # map_list: List[str] = [
+        #     "BerlingradAIE",
+        #     "InsideAndOutAIE",
+        #     "MoondanceAIE",
+        #     "StargazersAIE",
+        #     "WaterfallAIE",
+        #     "HardwireAIE",
+        # ]
 
         random_race = random.choice([Race.Zerg, Race.Terran, Race.Protoss])
         print("Starting local game...")
@@ -80,7 +72,7 @@ def main():
             maps.get(random.choice(map_list)),
             [
                 bot1,
-                bot2,
+                Computer(random_race, Difficulty.CheatVision, ai_build=AIBuild.Macro),
             ],
             realtime=False,
         )
